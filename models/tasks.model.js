e32fe303c754d02425477be373e4c62a7063bafc
hubertus.putu1 <hubertus.putu1@gmail.com>
2019-08-01 01:13:40 +0700

    add showData attr

--- models/tasks.model.js
+++ models/tasks.model.js
@@ -1,45 +1,60 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-import { STATUS_NEW, STATUS_ARCHIVED, STATUS_COMPLETED, STATUS_IN_PROGRESS } from '../utils/constants'
+import {
+	STATUS_NEW,
+	STATUS_ARCHIVED,
+	STATUS_COMPLETED,
+	STATUS_IN_PROGRESS
+} from '../utils/constants';
 
 const { Schema } = mongoose;
 
-const TasksSchema = new Schema({
-  assignedTo: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Users',
-    default: ''
-  },
-  status: {
-    type: String,
-    default: STATUS_NEW,
-    enum: [ STATUS_NEW, STATUS_ARCHIVED, STATUS_COMPLETED, STATUS_IN_PROGRESS ]
-  },
-  title: {
-    type: String,
-    required: true,
-    default: ''
-  },
-  description: {
-    type: String,
-    default: ''
-  },
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Users',
-    required: true
-  },
-}, {
-  timestamps: true
-});
+const TasksSchema = new Schema(
+	{
+		assignedTo: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Users',
+			default: ''
+		},
+		status: {
+			type: String,
+			default: STATUS_NEW,
+			enum: [
+				STATUS_NEW,
+				STATUS_ARCHIVED,
+				STATUS_COMPLETED,
+				STATUS_IN_PROGRESS
+			]
+		},
+		title: {
+			type: String,
+			required: true,
+			default: ''
+		},
+		description: {
+			type: String,
+			default: ''
+		},
+		createdBy: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Users',
+			required: true
+		}
+	},
+	{
+		timestamps: true
+	}
+);
 
 TasksSchema.methods.showData = function() {
-  return {
-    _id: this._id,
-    status: this.status,
-    title: this.title,
-    createdBy: this.createdBy
-  };
+	return {
+		_id: this._id,
+		status: this.status,
+		title: this.title,
+		description: this.description,
+		assignedTo: this.assignedTo,
+		createdBy: this.createdBy
+	};
 };
 
 mongoose.model('Tasks', TasksSchema);
