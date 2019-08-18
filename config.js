47b19eafa7bcb46eae174ffb3c47373ace6bd467
hubertus.putu1 <hubertus.putu1@gmail.com>
2019-08-18 17:10:54 +0700

    update config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 let config = {}
 
-config.PORT = process.env.PORT || '6000'
+config.PORT = process.env.PORT
 config.MONGODB_URI = process.env.MONGODB_URI
-config.API_PATH = process.env.API_PATH || 'api'
+config.API_PATH = process.env.API_PATH
 config.EMAIL_SERVICE = process.env.EMAIL_SERVICE
 config.EMAIL_USERNAME = process.env.EMAIL_USERNAME
 config.EMAIL_PASSWORD = process.env.EMAIL_PASSWORD
